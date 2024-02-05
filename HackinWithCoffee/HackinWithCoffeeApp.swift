ab3e6e91e50eb0b052d5c70984fe2c80476df753
Sergey Petrosyan <ipetriez@icloud.com>
2024-02-05 20:27:15 +0400

    Initial Commit

---
+++ HackinWithCoffee/HackinWithCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackinWithCoffeeApp.swift
+//  HackinWithCoffee
+//
+//  Created by Sergey Petrosyan on 05.02.24.
+//
+
+import SwiftUI
+
+@main
+struct HackinWithCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
